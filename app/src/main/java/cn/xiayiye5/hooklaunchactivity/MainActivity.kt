775d00aa896f53e8167491a2e736d721ebcc24e4
xiayiye5 <13343401268@qq.com>
2020-06-11 10:35:03 +0800

    添加对于APP启动页的Activity的说明

--- app/src/main/java/cn/xiayiye5/hooklaunchactivity/MainActivity.kt
+++ app/src/main/java/cn/xiayiye5/hooklaunchactivity/MainActivity.kt
@@ -3,6 +3,11 @@ package cn.xiayiye5.hooklaunchactivity
 import android.app.Activity
 import android.os.Bundle
 
+/**
+ * @author xiayiye5
+ * 2020年6月11日10:33:23
+ * 说明：此默认启动页一定要继承 Activity不然会报错，切记不能继承 AppCompatActivity会无法启动报错
+ */
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
